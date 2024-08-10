4d62941fa07d49f85287357e008e8fc7bdba7d41
Roohafza <97873081+Rooh123@users.noreply.github.com>
2024-08-10 05:23:14 +0430

    weather details API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ function resetWeather(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let speedElement = document.querySelector("#speed");
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function searchCity(city) {
